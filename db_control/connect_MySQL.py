3afb90c61c2de7bd293e9f7b57906fa1b59a9db4
Yuma Moriya <y.moriya.113@gmail.com>
2025-02-09 16:11:29 +0900

    SSL設定の追記

--- db_control/connect_MySQL.py
+++ db_control/connect_MySQL.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 if os.environ.get("APP_ENV", "development") == "development":
     load_dotenv()
 
+ssr_cert = str("DigiCertGlobalRootCA.crt")
+
 # データベース接続情報
 DB_USER = os.getenv('DB_USER')
 DB_PASSWORD = os.getenv('DB_PASSWORD')
@@ -15,12 +17,15 @@ DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
 
 # MySQLのURL構築
-DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # エンジンの作成
 engine = create_engine(
     DATABASE_URL,
     echo=True,
     pool_pre_ping=True,
-    pool_recycle=3600
+    pool_recycle=3600,
+    connect_args={
+        "ssl": {"ssl_ca": ssr_cert}
+    }
 )
